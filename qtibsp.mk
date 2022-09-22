9ced3d69f50e4ce4425ca2f4ce98e5c8d459aead
Bjordis Collaku <quic_bcollaku@quicinc.com>
2022-09-22 11:17:37 -0700

    owrt-qti-bsp: Add user_permissions to qtibsp.mk package group for default enablement.
    Change-Id: Id4e6586ae3f9cf565e74644b88c0131a7155a25e

--- qtibsp.mk
+++ qtibsp.mk
@@ -1,1 +1,1 @@
-QTIBSP:=adbd core-include ext4_utils fs_mgr libbase libcutils liblog libmincrypt mkbootimg libsparse logwrapper usb-composition edk2
+QTIBSP:=adbd core-include ext4_utils fs_mgr libbase libcutils liblog libmincrypt mkbootimg libsparse logwrapper usb-composition edk2 user_permissions
