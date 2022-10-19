bc96b4cc8d6808f9916170bd222740a18e8afcff
shoudil <quic_shoudil@quicinc.com>
2022-10-19 14:14:53 +0800

    owrt-qti-bsp: remove edk2 for target with split KP2.0
    Start with Kernel platform 2.0, edk2 will be included in KP internally.
    So remove edk2 for target using split KP2.0.
    Please note edk2 need to be removed from owrt product manifest to ensure
    it will be synced from KP side.
    
    Change-Id: Ia2b98338030da89cafaec845e2cca76cc9ab7538

--- qtibsp.mk
+++ qtibsp.mk
@@ -1,1 +1,6 @@
-QTIBSP:=adbd core-include ext4_utils fs_mgr libbase libcutils liblog libmincrypt mkbootimg libsparse logwrapper usb-composition edk2 user_permissions
+QTIBSP:=adbd core-include ext4_utils fs_mgr libbase libcutils liblog libmincrypt mkbootimg libsparse logwrapper usb-composition user_permissions
+
+## add target specific packages
+ifeq ($(BOARD),sdx65)
+	QTIBSP+= edk2
+endif
