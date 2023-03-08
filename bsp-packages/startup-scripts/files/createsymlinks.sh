0dd80f234e3edf82d810bf71f515319ab1c3cd97
Bjordis Collaku <quic_bcollaku@quicinc.com>
2023-03-07 16:28:27 -0800

    owrt-qti-bsp: Remove overlay on lib/modules & runtime creation of lib/modules/${uname -r}
    Change-Id: Ic72c52a67f9f5498730f8fdb1c4d38b4bb8cacf8

--- bsp-packages/startup-scripts/files/createsymlinks.sh
+++ bsp-packages/startup-scripts/files/createsymlinks.sh
@@ -99,10 +99,6 @@ mkdir -p /data/overlay-work
 mkdir -p /data/overlay-work/etc-upper
 mkdir -p /data/overlay-work/.etc-work
 mount -t overlay -o lowerdir=/etc,upperdir=/data/overlay-work/etc-upper,workdir=/data/overlay-work/.etc-work overlay /etc
-mkdir -p /data/overlay-work/lib-upper
-mkdir -p /data/overlay-work/.lib-work
-mount -t overlay -o lowerdir=/lib/modules,upperdir=/data/overlay-work/lib-upper,workdir=/data/overlay-work/.lib-work overlay /lib/modules
-mkdir -p /lib/modules/$(uname -r)
 
 # Need Restorecon for /persist & /firmware
 RESTORECON=/sbin/restorecon
