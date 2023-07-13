25e54869c3eea85dd07c62aa18abea25d3b576b1
Radhika Bandichhode <quic_rbandich@quicinc.com>
2023-07-13 09:35:58 -0700

    Added mounting scripts and ab-updater script
    Mount modem based on current active slot
    Enable ab-updater which executes --setsuccess during bootup

---
+++ bsp-packages/startup-scripts-ab/files/mounts-ab.sh
@@ -0,0 +1,104 @@
+#! /bin/sh
+
+# Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+FILES=/sys/class/block/
+
+CURRENT_SLOT=$(abctl --boot_slot)
+echo "$CURRENT_SLOT"
+
+create_symlinks()
+{
+        for file in $FILES/$1*
+        do
+                blockname=`basename $file`
+                if [  $1 == "mtd" ]; then
+                        partition_name=`cat $file/device/name`
+                else
+                        partition_name=`cat $file/uevent | awk '{ for ( n=1; n<=NF; n++ ) if($n ~ "PARTNAME") print $n }' | awk '{split($0,a, "=");print a[2]}'`
+                fi
+                mkdir -p /dev/block/bootdevice/by-name/
+                partition_name=/dev/block/bootdevice/by-name/$partition_name
+                target_dev=/dev/$blockname
+                ln -s $target_dev $partition_name
+        done
+}
+
+
+if [ ! -d /firmware/image ]; then
+        if [ -f /proc/mtd ] && [ `cat /proc/mtd | wc -l` -ge "2" ]; then
+
+                  mtd_block_number=`cat /proc/mtd | grep -i modem_a | sed 's/^mtd//' | awk -F ':' '{print $1}'`
+                  echo "MTD : Detected block device : firmware for modem_a "
+
+                  ubiattach -m $mtd_block_number -d 1 /dev/ubi_ctrl
+
+                  mtd_block_number=`cat /proc/mtd | grep -i modem_b | sed 's/^mtd//' | awk -F ':' '{print $1}'`
+                  echo "MTD : Detected block device : firmware for modem_b "
+
+                  ubiattach -m $mtd_block_number -d 2 /dev/ubi_ctrl
+
+                 device=/dev/ubi1_0
+                 if [ $CURRENT_SLOT == "_b" ]; then
+                        device=/dev/ubi2_0
+                 fi
+
+                 while [ 1 ]
+                 do
+                    if [ -c $device ];then
+                        mount -t ubifs $device /firmware  -o bulk_read,ro,context=u:r:qcfirmware.miscfile
+                        break
+                    else
+                        sleep 1
+                    fi
+                 done
+                create_symlinks mtd
+        else
+
+                if [ $CURRENT_SLOT == "_b" ]; then
+                        mount /dev/mmcblk0p2 /firmware
+                else
+                        mount /dev/mmcblk0p1 /firmware
+                fi
+                create_symlinks mmc
+        fi
+fi
+
+echo -n "/firmware/image" > /sys/module/firmware_class/parameters/path
+
+if [ -f /proc/mtd ] && [ `cat /proc/mtd | wc -l` -ge "2" ]; then
+        if [ ! -d /persist ]; then
+               echo "creating /persist"
+               mkdir -p /persist
+        fi
+        mount -t ubifs ubi0:persist /persist -o bulk_read
+        echo "persist is mounted to /persist"
+fi
+
+#Mount cache
+if [ ! -d /cache ]; then
+        echo "creating /cache dir"
+        mkdir -p /cache
+fi
+if [ -f /proc/mtd ] && [ `cat /proc/mtd | wc -l` -ge "2" ]; then
+        mount -t ubifs ubi0:cachefs /cache -o bulk_read
+else
+        mount -t ext4 /dev/block/bootdevice/by-name/cache /cache -o noatime,data=ordered,noauto_da_alloc,discard,noexec,nodev,nosuid,noauto
+fi
+
+# Mount userdata and overlayfs
+if [ -f /proc/mtd ] && [ `cat /proc/mtd | wc -l` -ge "2" ]; then
+        mount -t ubifs /dev/ubi0_1 /data -o bulk_read,rw
+else
+        mount -t ext4 /dev/block/bootdevice/by-name/userdata /data
+fi
+
+mkdir -p /data/overlay-work$CURRENT_SLOT
+mkdir -p /data/overlay-work/etc-upper$CURRENT_SLOT
+mkdir -p /data/overlay-work/.etc-work$CURRENT_SLOT
+mount -t overlay -o lowerdir=/etc,upperdir=/data/overlay-work/etc-upper$CURRENT_SLOT,workdir=/data/overlay-work/.etc-work$CURRENT_SLOT overlay /etc
+
+# Need Restorecon for /persist & /firmware
+RESTORECON=/sbin/restorecon
+${RESTORECON} -RF /persist /data /etc
