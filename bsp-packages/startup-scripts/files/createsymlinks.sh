be36a9d77d1a2ba683674669f72d1280d028389c
Mohamed Aslam M <mohasl@qti.qualcomm.com>
2025-11-04 13:40:16 +0530

    owrt-qti-bsp: Mount lcm_data and securestore partitions for prplOs
    Change-Id: If49fb91908ff54b0a948f18e507b536c69fa90ee

--- bsp-packages/startup-scripts/files/createsymlinks.sh
+++ bsp-packages/startup-scripts/files/createsymlinks.sh
@@ -106,6 +106,11 @@ else
         mount -t ext4 /dev/block/bootdevice/by-name/systemrw /overlay -o noatime,data=ordered,noauto_da_alloc,discard,noexec,nodev,nosuid,noauto
         mount -t ext4 /dev/block/bootdevice/by-name/userdata /data
         mount -t ext4 /dev/block/bootdevice/by-name/persist /persist -o noatime,data=ordered,noauto_da_alloc,discard,noexec,nodev,nosuid,noauto,context=u:r:persist.miscfile
+        if [[ -f /etc/os-release ]] && grep -qi "prplOs" /etc/os-release 2>/dev/null; then
+            mount -t ext4 /dev/block/bootdevice/by-name/lcm_data /lcm
+            mount -t ext4 /dev/block/bootdevice/by-name/securestore /cfg
+        fi
+
 fi
 
 if [ $soc_id == "570" ] || [ $soc_id == "571" ] || [ $soc_id == "717" ] || [ $soc_id == "738" ]; then
