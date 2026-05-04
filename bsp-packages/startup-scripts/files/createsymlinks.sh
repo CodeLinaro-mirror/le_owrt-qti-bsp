d5b0bfb82c63801b17c84cb727d93963cfea81e5
Mohamed Aslam M <mohasl@qti.qualcomm.com>
2026-05-04 14:33:21 +0530

    boot: optimise create-symlinks.sh for faster boot
    Remove redundant code and repeated checks in create-symlinks.sh to
    reduce boot-time overhead.
    
    Change-Id: Ieeb7ca5c9244a5786649375a5dca059cc0ce9fe4

--- bsp-packages/startup-scripts/files/createsymlinks.sh
+++ bsp-packages/startup-scripts/files/createsymlinks.sh
@@ -4,7 +4,7 @@
 #SPDX-License-Identifier: BSD-3-Clause-Clear
 
 source /usr/bin/mount-userdata.sh
-FILES=/sys/class/block/
+FILES=/sys/class/block
 exec >> /dev/kmsg 2>&1
 
 # SELinux context options (cleared for prpl builds)
@@ -21,42 +21,65 @@ if [ -f /etc/os-release ] && grep -qi "prplOs" /etc/os-release 2>/dev/null; then
     firmware_context=""
 fi
 
+has_mtd=0
+if [ -f /proc/mtd ]; then
+    mtd_lines=$(wc -l < /proc/mtd)
+    [ "$mtd_lines" -ge 2 ] && has_mtd=1
+fi
+
+soc_id=$(cat /sys/devices/soc0/soc_id 2>/dev/null)
+
+is_overlay_on_data=0
+case "$soc_id" in
+    570|571|717|738) is_overlay_on_data=1 ;;
+esac
+
+get_mtd_num()
+{
+        awk -v name="$1" -F'[: ]+' '
+        tolower($0) ~ tolower(name) { sub(/^mtd/, "", $1); print $1; exit }
+        ' /proc/mtd
+}
+
 create_symlinks()
 {
+        mkdir -p /dev/block/bootdevice/by-name
         for file in $FILES/$1*
         do
-                blockname=`basename $file`
-                if [  $1 == "mtd" ]; then
-                        partition_name=`cat $file/device/name`
+                blockname=$(basename "$file")
+
+                if [ $1 == "mtd" ]; then
+                       read -r partition_name < "$file/device/name"
                 else
-                        partition_name=`cat $file/uevent | awk '{ for ( n=1; n<=NF; n++ ) if($n ~ "PARTNAME") print $n }' | awk '{split($0,a, "=");print a[2]}'`
+                       partition_name=$(
+                       awk -F= '/^PARTNAME=/{print $2; exit}' "$file/uevent" 2>/dev/null
+                       )
                 fi
-                mkdir -p /dev/block/bootdevice/by-name/
-                partition_name=/dev/block/bootdevice/by-name/$partition_name
-                target_dev=/dev/$blockname
-                ln -s $target_dev $partition_name
+
+                [ -n "$partition_name" ] || continue
+                ln -sf "/dev/$blockname" "/dev/block/bootdevice/by-name/$partition_name"
         done
 }
 
 
 if [ ! -d /firmware/image ]; then
-        if [ -f /proc/mtd ] && [ `cat /proc/mtd | wc -l` -ge "2" ]; then
-                mtd_block_number=`cat /proc/mtd | grep -i modem | sed 's/^mtd//' | awk -F ':' '{print $1}'`
-                echo "MTD : Detected block device : firmware for modem "
-                mkdir -p $dir
+        if [ "$has_mtd" -eq 1 ]; then
+                mtd_block_number=$(get_mtd_num "modem")
+                echo "MTD : Detected block device : firmware for modem"
 
                 ubiattach -m $mtd_block_number -d 1 /dev/ubi_ctrl
                 device=/dev/ubi1_0
-                while [ 1 ]
-                do
-                    if [ -c $device ]
-					then
-                        mount -t ubifs /dev/ubi1_0 /firmware  -o bulk_read,ro$firmware_context
-                        break
-					else
-                        sleep 1
-                    fi
+
+                retry=0
+                while [ ! -c "$device" ] && [ "$retry" -lt 10 ]; do
+                sleep 1
+                retry=$((retry + 1))
                 done
+
+                if [ -c $device ]; then
+                        mount -t ubifs $device /firmware -o bulk_read,ro$firmware_context
+                fi
+
                 create_symlinks mtd
         else
 		mount /dev/mmcblk0p1 /firmware -o ro$firmware_context
@@ -66,11 +89,8 @@ fi
 
 echo -n "/firmware/image" > /sys/module/firmware_class/parameters/path
 
-if [ -f /proc/mtd ] && [ `cat /proc/mtd | wc -l` -ge "2" ]; then
-	if [ ! -d /persist ]; then
-		echo "creating /persist"
-		mkdir -p /persist
-	fi
+if [ "$has_mtd" -eq 1 ]; then
+	[ -d /persist ] || mkdir -p /persist
 	mount -t ubifs ubi0:persist /persist -o bulk_read
 	echo "persist is mounted to /persist"
 fi
@@ -81,10 +101,10 @@ if [ ! -d /cache ]; then
         mkdir -p /cache
 fi
 
-soc_id=`cat /sys/devices/soc0/soc_id`
-if [ -f /proc/mtd ] && [ `cat /proc/mtd | wc -l` -ge "2" ]; then
+if [ "$has_mtd" -eq 1 ]; then
         mount -t ubifs ubi0:cachefs /cache -o bulk_read$cache_context
-        if [ $soc_id != "570" ] && [ $soc_id != "571" ] && [ $soc_id != "717" ] && [ $soc_id != "738" ]; then
+
+        if [ "$is_overlay_on_data" -ne 1 ]; then
             mount -t ubifs ubi0:systemrw /overlay -o bulk_read
         fi
         mount -t ubifs /dev/ubi0_1 /data -o bulk_read,rw
@@ -92,52 +112,51 @@ else
         mount -t ext4 /dev/block/bootdevice/by-name/cache /cache -o noatime,data=ordered,noauto_da_alloc,discard,noexec,nodev,nosuid,noauto$cache_context
         mount -t ext4 /dev/block/bootdevice/by-name/systemrw /overlay -o noatime,data=ordered,noauto_da_alloc,discard,noexec,nodev,nosuid,noauto
         mount -t ext4 /dev/block/bootdevice/by-name/persist /persist -o noatime,data=ordered,noauto_da_alloc,discard,noexec,nodev,nosuid,noauto$persist_context
-        if [ $soc_id == "570" ] || [ $soc_id == "571" ] || [ $soc_id == "717" ] || [ $soc_id == "738" ]; then
+        if [ "$is_overlay_on_data" -eq 1 ]; then
             mount -t ext4 /dev/block/bootdevice/by-name/userdata /data
         fi
 
 fi
 
-if [ $soc_id == "570" ] || [ $soc_id == "571" ] || [ $soc_id == "717" ] || [ $soc_id == "738" ]; then
-    mkdir -p /data/overlay-work
-    mkdir -p /data/overlay-work/etc-upper
-    mkdir -p /data/overlay-work/.etc-work
+if [ "$is_overlay_on_data" -eq 1 ]; then
+    mkdir -p /data/overlay-work/etc-upper /data/overlay-work/.etc-work
     chcon -t file.conffile /data/overlay-work/.etc-work
     mount -t overlay -o lowerdir=/etc,upperdir=/data/overlay-work/etc-upper,workdir=/data/overlay-work/.etc-work overlay /etc
 else
-    mkdir -p /overlay/etc-upper
-    mkdir -p /overlay/.etc-work
+    mkdir -p /overlay/etc-upper /overlay/.etc-work
+
     if [ "$prplos_build" -ne 1 ]; then
     chcon -t file.conffile /overlay/.etc-work
     fi
+
     mount -t overlay -o lowerdir=/etc,upperdir=/overlay/etc-upper,workdir=/overlay/.etc-work overlay /etc
-    if [ -f /proc/mtd ] && [ `cat /proc/mtd | wc -l` -ge "2" ]; then
-       echo "usrfs is a logical volume on ubi0"
+
+    if [ "$has_mtd" -ne 1 ]; then
+        : > /cache/lvm.log
+        {
+            create_lvm
+            mount_userdata
+        } >> /cache/lvm.log 2>&1
+        setup_ext_bind_mount
     else
-       : > "/cache/lvm.log"
-    {
-       create_lvm
-       mount_userdata
-    } >>"/cache/lvm.log" 2>&1
-    setup_ext_bind_mount
+        echo "usrfs is a logical volume on ubi0"
     fi
 fi
+
 if [ "$prplos_build" -ne 1 ]; then
-RESTORECON=/sbin/restorecon
-${RESTORECON} -R  /etc -e /etc/rc.d
+    RESTORECON=/sbin/restorecon
+    "$RESTORECON" -R /etc -e /etc/rc.d
 
 # For  Boot KPI we will call restorcon only  for the first boot
 # any new files will get labeled on creation
 
-if [ ! -f /persist/.autolabeled ]; then
-     # Need Restorecon for /persist
-     ${RESTORECON} -RF /persist
-     touch  /persist/.autolabeled
-fi
+    if [ ! -f /persist/.autolabeled ]; then
+        "$RESTORECON" -RF /persist
+        touch /persist/.autolabeled
+    fi
 
-if [ ! -f /data/.autolabeled ]; then
-        # Need Restorecon for /data
-        ${RESTORECON} -RF /data
-        touch  /data/.autolabeled
-fi
+    if [ ! -f /data/.autolabeled ]; then
+        "$RESTORECON" -RF /data
+        touch /data/.autolabeled
+    fi
 fi
